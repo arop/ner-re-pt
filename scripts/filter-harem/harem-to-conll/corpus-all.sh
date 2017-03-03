074a6c274292dbf34b27e0f1c84ace37dbdb9e48
arop <andre.r.o.pires@gmail.com>
2017-03-03 20:50:14 +0000

    split train and test

--- scripts/filter-harem/harem-to-conll/corpus-all.sh
+++ scripts/filter-harem/harem-to-conll/corpus-all.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-corpus-processor process "../outputs/harem-filtered.xml" "output/filtered" --categories="ymls/filtered.yml"
+corpus-processor process "../outputs/filtered_all.xml" "output/filtered_all" --categories="ymls/filtered.yml"
+corpus-processor process "../outputs/filtered_train.xml" "output/filtered_train" --categories="ymls/filtered.yml"
+corpus-processor process "../outputs/filtered_test.xml" "output/filtered_test" --categories="ymls/filtered.yml"
 
-corpus-processor process "../outputs/harem-only-categories.xml" "output/only-cat" --categories="ymls/cats.yml"
+corpus-processor process "../outputs/cat_all.xml" "output/cat_all" --categories="ymls/cats.yml"
+corpus-processor process "../outputs/cat_train.xml" "output/cat_train" --categories="ymls/cats.yml"
+corpus-processor process "../outputs/cat_test.xml" "output/cat_test" --categories="ymls/cats.yml"
 
-corpus-processor process "../outputs/harem-only-types.xml" "output/only-type" --categories="ymls/types.yml"
+corpus-processor process "../outputs/types_all.xml" "output/types_all" --categories="ymls/types.yml"
+corpus-processor process "../outputs/types_train.xml" "output/types_train" --categories="ymls/types.yml"
+corpus-processor process "../outputs/types_test.xml" "output/types_test" --categories="ymls/types.yml"
 
-corpus-processor process "../outputs/harem-only-subtypes.xml" "output/only-subtype" --categories="ymls/subtypes.yml"
+corpus-processor process "../outputs/subtypes_all.xml" "output/subtypes_all" --categories="ymls/subtypes.yml"
+corpus-processor process "../outputs/subtypes_train.xml" "output/subtypes_train" --categories="ymls/subtypes.yml"
+corpus-processor process "../outputs/subtypes_test.xml" "output/subtypes_test" --categories="ymls/subtypes.yml"
---
+++ scripts/filter-harem/src/split_train_test.py
@@ -0,0 +1,40 @@
+from lxml import etree
+import sys
+#129 documents
+#90 train
+#39 test
+
+if(len(sys.argv) > 1):
+  harem_filename = sys.argv[1]
+  output_name = sys.argv[2]
+else:
+  print "Usage: python split_train_test.py <input file> <output file>"
+  sys.exit()
+
+tree = etree.parse(harem_filename)
+train = tree.xpath("//DOC[position() <= 90]")
+test = tree.xpath("//DOC[position() > 90]")
+
+out_tree_str = "<?xml version='1.0' encoding='ISO-8859-1'?>\n"
+out_tree_str += "<!DOCTYPE colHAREM>\n"
+out_tree_str += "<colHAREM versao=\"Segundo_dourada_com_relacoes_14Abril2010\">\n"
+out_tree_str += "</colHAREM>"
+
+out_tree_train = etree.fromstring(out_tree_str)
+out_tree_test = etree.fromstring(out_tree_str)
+
+for el in train:
+	out_tree_train.append(el)
+
+for el in test:
+	out_tree_test.append(el)
+
+# output to file
+f = open("outputs/"+output_name+"_train.xml",'w')
+f.write(etree.tostring(out_tree_train, encoding="ISO-8859-1"))
+f.close()
+
+# output to file
+f = open("outputs/"+output_name+"_test.xml",'w')
+f.write(etree.tostring(out_tree_test, encoding="ISO-8859-1"))
+f.close()
