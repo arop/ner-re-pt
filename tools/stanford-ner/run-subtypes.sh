95e655daa9883f4aec2f5c5e50ec68a0306cdf3a
arop <andre.r.o.pires@gmail.com>
2017-03-14 18:12:30 +0000

    ran stanford-nlp for types and subtypes with less features

--- tools/stanford-ner/run-subtypes.sh
+++ tools/stanford-ner/run-subtypes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-java -d64 -Xmx6g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -prop "props/subtypes.prop"
+java -d64 -Xmx14g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -prop "props/subtypes.prop"
 
-java -d64 -Xmx6g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier subtypes-ner-model.ser.gz -testFile "../../scripts/filter-harem/harem-to-stanford/output/cat_test"
+java -d64 -Xmx14g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier subtypes-ner-model.ser.gz -testFile "../../scripts/filter-harem/harem-to-stanford/output/subtypes_test"
--- tools/stanford-ner/run-types.sh
+++ tools/stanford-ner/run-types.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-java -d64 -Xms6g -Xmx7g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -prop "props/types.prop"
+java -d64 -Xms6g -Xmx14g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -prop "props/types.prop"
 
-java -d64 -Xms6g -Xmx7g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier types-ner-model.ser.gz -testFile "../../scripts/filter-harem/harem-to-stanford/output/cat_test"
+java -d64 -Xms6g -Xmx14g -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier types-ner-model.ser.gz -testFile "../../scripts/filter-harem/harem-to-stanford/output/types_test"
