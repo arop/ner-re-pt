3ebb756d3ad3827aa2c8f57773b3c5c2edda105c
arop <andre.r.o.pires@gmail.com>
2017-03-19 23:19:04 +0000

    folds

--- scripts/filter-harem/run-scripts/remove_all_unwanted_spaces.sh
+++ scripts/filter-harem/run-scripts/remove_all_unwanted_spaces.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 python "../src/remove_unwanted_spaces.py" "../outputs/cat_all.xml" "cat_all.xml"
-python "../src/remove_unwanted_spaces.py" "../outputs/cat_train.xml" "cat_train.xml"
-python "../src/remove_unwanted_spaces.py" "../outputs/cat_test.xml" "cat_test.xml"
-
 python "../src/remove_unwanted_spaces.py" "../outputs/types_all.xml" "types_all.xml"
-python "../src/remove_unwanted_spaces.py" "../outputs/types_train.xml" "types_train.xml"
-python "../src/remove_unwanted_spaces.py" "../outputs/types_test.xml" "types_test.xml"
-
 python "../src/remove_unwanted_spaces.py" "../outputs/subtypes_all.xml" "subtypes_all.xml"
-python "../src/remove_unwanted_spaces.py" "../outputs/subtypes_train.xml" "subtypes_train.xml"
-python "../src/remove_unwanted_spaces.py" "../outputs/subtypes_test.xml" "subtypes_test.xml"
-
 python "../src/remove_unwanted_spaces.py" "../outputs/filtered_all.xml" "filtered_all.xml"
-python "../src/remove_unwanted_spaces.py" "../outputs/filtered_train.xml" "filtered_train.xml"
-python "../src/remove_unwanted_spaces.py" "../outputs/filtered_test.xml" "filtered_test.xml"
+
+for i in {0..9}
+do
+	python "../src/remove_unwanted_spaces.py" "../outputs/fold-"$i"/cat_train.xml" "fold-"$i"/cat_train.xml"
+	python "../src/remove_unwanted_spaces.py" "../outputs/fold-"$i"/cat_test.xml" "fold-"$i"/cat_test.xml"
+
+	python "../src/remove_unwanted_spaces.py" "../outputs/fold-"$i"/types_train.xml" "fold-"$i"/types_train.xml"
+	python "../src/remove_unwanted_spaces.py" "../outputs/fold-"$i"/types_test.xml" "fold-"$i"/types_test.xml"
+
+	python "../src/remove_unwanted_spaces.py" "../outputs/fold-"$i"/subtypes_train.xml" "fold-"$i"/subtypes_train.xml"
+	python "../src/remove_unwanted_spaces.py" "../outputs/fold-"$i"/subtypes_test.xml" "fold-"$i"/subtypes_test.xml"
+
+	python "../src/remove_unwanted_spaces.py" "../outputs/fold-"$i"/filtered_train.xml" "fold-"$i"/filtered_train.xml"
+	python "../src/remove_unwanted_spaces.py" "../outputs/fold-"$i"/filtered_test.xml" "fold-"$i"/filtered_test.xml"
+done
