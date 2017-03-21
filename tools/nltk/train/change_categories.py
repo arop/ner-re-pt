8f4cc1ab584c71f4e98042b7f45d1c739b9092fb
arop <andre.r.o.pires@gmail.com>
2017-03-21 18:00:06 +0000

    changing chunk types by script

---
+++ tools/nltk/train/change_categories.py
@@ -0,0 +1,27 @@
+import re
+import sys
+
+if(len(sys.argv) > 1):
+  level = sys.argv[1]
+  if level not in ['cat', 'types', 'subtypes', 'filtered']:
+  	print "ERROR: wrong level! Use cat or types or subtypes or filtered.\n"
+  	sys.exit()	
+else:
+  print "Usage: python " + sys.argv[0] + " <level>\n"
+  sys.exit()
+
+file = 'nltk_trainer/__init__.py'
+
+f = open(file,'r')
+file_str = f.read()
+f.close()
+
+pattern = re.compile(r'########### HERE ############\n\t\tkwargs\[\'chunk_types\'\] = \w+')
+
+replacement = "########### HERE ############\n\t\tkwargs['chunk_types'] = "+ level
+
+file_str = re.sub(pattern, replacement, file_str)
+
+f = open(file,'w')
+f.write(file_str)
+f.close()
--- tools/nltk/train/run-nltk-train-conll.sh
+++ tools/nltk/train/run-nltk-train-conll.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-# CATEGORIES
-#python train_chunker.py corpora/harem --fileids 'out-conll-cat-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier NaiveBayes --fraction 0.75
-#python train_chunker.py corpora/harem --fileids 'out-conll-cat-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier Maxent --fraction 0.75
-#python train_chunker.py corpora/harem --fileids 'out-conll-cat-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier DecisionTree --fraction 0.75
-#python train_chunker.py corpora/harem --fileids 'out-conll-cat-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --sequential 'ubt' --fraction 0.75
+READER=nltk.corpus.reader.conll.ConllChunkCorpusReader
+for i in {0..9}
+do
+	printf "\n** fold "$i" **\n"
+	FOLDER=../outputs/fold-$i
+	MODEL=./models/fold-$i
+	# CATEGORIES
+	#iconv -f ISO-8859-1 -t UTF-8 $FOLDER/out-conll-cat.txt > ../outputs/fold-$i/out-conll-cat-utf8.txt
+	#python change_categories.py cat
+	#python train_chunker.py $FOLDER --fileids 'out-conll-cat-utf8.txt' --filename $MODEL/cat_NaiveBayes --reader $READER --classifier NaiveBayes
+	#python train_chunker.py $FOLDER --fileids 'out-conll-cat-utf8.txt' --filename $MODEL/cat_Maxent --reader $READER --classifier Maxent
+	#python train_chunker.py $FOLDER --fileids 'out-conll-cat-utf8.txt' --filename $MODEL/cat_DecisionTree --reader $READER --classifier DecisionTree
 
-# TYPES
-#python train_chunker.py corpora/harem --fileids 'out-conll-types-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier NaiveBayes --fraction 0.75
-#python train_chunker.py corpora/harem --fileids 'out-conll-types-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier Maxent --fraction 0.75
-#python train_chunker.py corpora/harem --fileids 'out-conll-types-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier DecisionTree --fraction 0.75
-#python train_chunker.py corpora/harem --fileids 'out-conll-types-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --sequential 'ubt' --fraction 0.75
+	# TYPES
+	#iconv -f ISO-8859-1 -t UTF-8 $FOLDER/out-conll-types.txt > $FOLDER/out-conll-types-utf8.txt
+	#python change_categories.py types
+	#python train_chunker.py $FOLDER --fileids 'out-conll-types-utf8.txt' --filename $MODEL/types_NaiveBayes --reader $READER --classifier NaiveBayes
+	#python train_chunker.py $FOLDER --fileids 'out-conll-types-utf8.txt' --filename $MODEL/types_Maxent --reader $READER --classifier Maxent
+	#python train_chunker.py $FOLDER --fileids 'out-conll-types-utf8.txt' --filename $MODEL/types_DecisionTree --reader $READER --classifier DecisionTree
 
-# SUBTYPES
-#python train_chunker.py corpora/harem --fileids 'out-conll-subtypes-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier NaiveBayes --fraction 0.75
-#python train_chunker.py corpora/harem --fileids 'out-conll-subtypes-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier Maxent --fraction 0.75
-#python train_chunker.py corpora/harem --fileids 'out-conll-subtypes-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier DecisionTree --fraction 0.75
-#python train_chunker.py corpora/harem --fileids 'out-conll-subtypes-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --sequential 'ubt' --fraction 0.75
+	# SUBTYPES
+	#iconv -f ISO-8859-1 -t UTF-8 $FOLDER/out-conll-subtypes.txt > $FOLDER/out-conll-subtypes-utf8.txt
+	#python change_categories.py subtypes
+	#python train_chunker.py $FOLDER --fileids 'out-conll-subtypes-utf8.txt' --filename $MODEL/subtypes_NaiveBayes --reader $READER --classifier NaiveBayes
+	#python train_chunker.py $FOLDER --fileids 'out-conll-subtypes-utf8.txt' --filename $MODEL/subtypes_Maxent --reader $READER --classifier Maxent
+	#python train_chunker.py $FOLDER --fileids 'out-conll-subtypes-utf8.txt' --filename $MODEL/subtypes_DecisionTree --reader $READER --classifier DecisionTree
 
-# FILTERED
-#python train_chunker.py corpora/harem --fileids 'out-conll-filtered-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier NaiveBayes --fraction 0.75
-#python train_chunker.py corpora/harem --fileids 'out-conll-filtered-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier Maxent --fraction 0.75
-python train_chunker.py corpora/harem --fileids 'out-conll-filtered-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --classifier DecisionTree --fraction 0.75
-#python train_chunker.py corpora/harem --fileids 'out-conll-filtered-utf8.txt' --reader nltk.corpus.reader.conll.ConllChunkCorpusReader --sequential 'ubt' --fraction 0.75
+	# FILTERED
+	iconv -f ISO-8859-1 -t UTF-8 $FOLDER/out-conll-filtered.txt > $FOLDER/out-conll-filtered-utf8.txt
+	python change_categories.py filtered
+	python train_chunker.py $FOLDER --fileids 'out-conll-filtered-utf8.txt' --filename $MODEL/filtered_NaiveBayes --reader $READER --classifier NaiveBayes
+	python train_chunker.py $FOLDER --fileids 'out-conll-filtered-utf8.txt' --filename $MODEL/filtered_Maxent --reader $READER --classifier Maxent
+	python train_chunker.py $FOLDER --fileids 'out-conll-filtered-utf8.txt' --filename $MODEL/filtered_DecisionTree --reader $READER --classifier DecisionTree
+done
