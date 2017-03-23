5de992027c03805476a4ab2114c8d961c1da6f20
arop <andre.r.o.pires@gmail.com>
2017-03-23 16:58:47 +0000

    changed check equal opennlp

---
+++ tools/open-nlp/outputs/check-equal.py
@@ -0,0 +1,20 @@
+import sys
+
+if len(sys.argv) > 3:
+	level = sys.argv[1]
+	fold = sys.argv[2]
+	repeat = sys.argv[3]
+else:
+	print "Usage: " + sys.argv[0] + " <level> <fold> <repeat>\n"
+	sys.exit()
+
+one = open('repeat-'+repeat+'/ner-results/fold-'+fold+'/out-'+level+'.txt','r').read().splitlines()
+two = open('repeat-'+repeat+'/ner-results/fold-'+fold+'/out-'+level+'-gold.txt','r').read().splitlines()
+
+i = 0
+for i,line in enumerate(one):
+	if not line.split()[0] == two[i].split()[0]:
+		print "ERROR: " + line.split()[0] + " vs. " + two[i].split()[0] + " | line: " + str(i)
+		print "Fold: " + fold + " | Level: " + level
+		break
+	i += 1
