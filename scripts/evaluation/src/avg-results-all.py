50440dc2289bd052e7f5a0ca45e3fb295feede9d
arop <andre.r.o.pires@gmail.com>
2017-03-23 00:41:11 +0000

    all opennlp completed, added script to calculate avg from repeats

---
+++ scripts/evaluation/src/avg-results-all.py
@@ -0,0 +1,113 @@
+import sys
+
+if(len(sys.argv) > 2):
+  tool = sys.argv[1]
+  level = sys.argv[2]
+else:
+  print "Usage: python " + sys.argv[0] + " <tool> <level>\n"
+  sys.exit()
+
+
+def get_avg(l):
+	if len(l) == 0:
+		return 0.0
+	return reduce(lambda x, y: x + y, l) / float(len(l))
+
+def get_accuracy(str):
+	chunks = str.split()
+	out = []
+
+	out.append(("accuracy",float( chunks[7] )))
+	out.append(("precision",float( chunks[1] )))
+	out.append(("recall",float( chunks[3] )))
+	out.append(("FB1",float( chunks[5] )))
+	return out
+
+def get_level_result(str):
+	chunks = str.split()
+	out = []
+	cats = chunks[6]
+
+	out.append(("category", chunks[6] ))
+	out.append(("precision",float( chunks[1] )))
+	out.append(("recall",float( chunks[3] )))
+	out.append(("FB1",float( chunks[5] )))
+	return out, cats
+
+
+#####################################
+
+results_files = []
+for i in range(4):
+	f = open('../results/' + tool + '/repeat-' + str(i) + '/avg/' + level + '-avg.txt', 'r')
+	results_files.append(f.read())
+	f.close()
+
+
+results = []
+cats = []
+for results_file in results_files:
+	lines = results_file.splitlines()
+	result = []
+	result.append(get_accuracy(lines[0]))
+
+	for line in lines[1:]:
+		l = get_level_result(line)
+		result.append(l[0])
+		cats.append(l[1])
+
+	results.append(result)
+
+cats = list(set(cats))
+
+##########
+## AVGs ##
+
+g_acc = []
+g_p = []
+g_r = []
+g_fb1 = []
+
+cats = dict([(i , [[],[],[]]) for i in cats])
+
+for result in results:
+	acc_line = result[0]
+
+	g_acc.append(acc_line[0][1]) # get accuracy value
+	g_p.append(acc_line[1][1]) # get p value
+	g_r.append(acc_line[2][1]) # get r value
+	g_fb1.append(acc_line[3][1]) # get fb1 value
+
+	for cat in range(len(cats)):
+		try:
+			name = result[cat+1][0][1]
+			#cats[name][0] = result[cat+1][0][1] # get category name
+			cats[name][0].append(result[cat+1][1][1]) # get p
+			cats[name][1].append(result[cat+1][2][1]) # get p
+			cats[name][2].append(result[cat+1][3][1]) # get p
+		except IndexError:
+			#print "One fold (" + str(i) + ") does not have all categories!"
+			continue #already dealt with
+
+avg_g_acc = get_avg(g_acc)
+avg_g_p = get_avg(g_p)
+avg_g_r = get_avg(g_r)
+avg_g_fb1 = get_avg(g_fb1)
+
+to_file = ""
+
+to_file += "precision: {:04.2f}".format(avg_g_p) 
+to_file += "\trecall: {:04.2f}".format(avg_g_r) 
+to_file += "\tFB1: {:04.2f}".format(avg_g_fb1) 
+to_file += "\taccuracy: {:04.2f}".format(avg_g_acc) + '\n'
+
+for cat in cats:
+	to_file += "precision: {:05.2f}".format(get_avg(cats[cat][0])) 
+	to_file += "\trecall: {:05.2f}".format(get_avg(cats[cat][1])) 
+	to_file += "\tFB1: {:05.2f}".format(get_avg(cats[cat][2])) 
+	to_file += '\t' + cat + '\n'
+
+f = open('../results/' + tool + '/avg/' + level + '-avg.txt', 'w')
+f.write(to_file)
+f.close()
+
