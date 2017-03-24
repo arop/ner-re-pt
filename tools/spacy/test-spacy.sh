f36277e485847c556e495c5b04f606aded326d01
arop <andre.r.o.pires@gmail.com>
2017-03-24 00:53:38 +0000

    minor change in spacy

--- tools/spacy/test-spacy.sh
+++ tools/spacy/test-spacy.sh
@@ -11,5 +11,10 @@ do
 		python "src/ner-test-spacy.py" $FOLDER/"cat_test-standoff" repeat-$r/fold-$i/types
 		python "src/ner-test-spacy.py" $FOLDER/"cat_test-standoff" repeat-$r/fold-$i/subtypes
 		python "src/ner-test-spacy.py" $FOLDER/"cat_test-standoff" repeat-$r/fold-$i/filtered
+
+		iconv -f UTF-8 -t ISO-8859-1 outputs/repeat-$r/fold-$i/cat-gold.txt outputs/repeat-$r/fold-$i/cat-gold.txt
+		iconv -f UTF-8 -t ISO-8859-1 outputs/repeat-$r/fold-$i/types-gold.txt outputs/repeat-$r/fold-$i/types-gold.txt
+		iconv -f UTF-8 -t ISO-8859-1 outputs/repeat-$r/fold-$i/subtypes-gold.txt outputs/repeat-$r/fold-$i/subtypes-gold.txt
+		iconv -f UTF-8 -t ISO-8859-1 outputs/repeat-$r/fold-$i/filtered-gold.txt outputs/repeat-$r/fold-$i/filtered-gold.txt
 	done
 done
