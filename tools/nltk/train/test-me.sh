2315572971da4546e06a40697210108dae67ef99
arop <andre.r.o.pires@gmail.com>
2017-05-18 00:47:42 +0100

    nltk me ready for more

--- tools/nltk/train/test-me.sh
+++ tools/nltk/train/test-me.sh
@@ -25,6 +25,23 @@ do
 	# done
 
 	# min_lldelta
+	# for i in 0 0.0000001 0.000001 0.00001 0.0001 0.001 0.01 0.05 0.1 0.15 0.2
+	# do
+	# 	printf "\n** min_lldelta "$i" **\n"
+
+	# 	for level in "${levels[@]}"
+	# 	do
+	# 		OUT=../outputs/repeat-$r/joined
+	# 		RESULTS=../outputs/repeat-$r/ner-results/experiences/me_min_lldelta/$i
+	# 		MODELS=models/repeat-$r/experiences/me_min_lldelta/$i
+
+	# 		iconv -f ISO-8859-1 -t UTF-8 $OUT/pos-$level-docs-test.txt > $OUT/pos-$level-docs-test-utf8.txt
+	# 		python $TEST $MODELS/$level"_Maxent.pickle" $OUT/pos-$level-docs-test-utf8.txt $RESULTS/out-$level-ME.txt
+	# 	done
+	# done
+
+	# min_lldelta with max_iter = 100
+	printf "\n** max_iter = 100 **\n"
 	for i in 0 0.0000001 0.000001 0.00001 0.0001 0.001 0.01 0.05 0.1 0.15 0.2
 	do
 		printf "\n** min_lldelta "$i" **\n"
@@ -32,8 +49,8 @@ do
 		for level in "${levels[@]}"
 		do
 			OUT=../outputs/repeat-$r/joined
-			RESULTS=../outputs/repeat-$r/ner-results/experiences/me_min_lldelta/$i
-			MODELS=models/repeat-$r/experiences/me_min_lldelta/$i
+			RESULTS=../outputs/repeat-$r/ner-results/experiences/me_min_lldelta-iter-50/$i
+			MODELS=models/repeat-$r/experiences/me_min_lldelta-iter-50/$i
 
 			iconv -f ISO-8859-1 -t UTF-8 $OUT/pos-$level-docs-test.txt > $OUT/pos-$level-docs-test-utf8.txt
 			python $TEST $MODELS/$level"_Maxent.pickle" $OUT/pos-$level-docs-test-utf8.txt $RESULTS/out-$level-ME.txt
--- tools/nltk/train/train-me.sh
+++ tools/nltk/train/train-me.sh
@@ -26,17 +26,35 @@ do
 	# done
 
 	# min_lldelta
+	# for i in 0 0.0000001 0.000001 0.00001 0.0001 0.001 0.01 0.05 0.1 0.15 0.2
+	# do
+	# 	printf "\n** min_lldelta "$i" **\n"
+	# 	FOLDER=../outputs/repeat-$r/joined
+	# 	MODEL=./models/repeat-$r/experiences/me_min_lldelta/$i
+		
+	# 	for level in "${levels[@]}"
+	# 	do
+	# 		iconv -f ISO-8859-1 -t UTF-8 $FOLDER/out-conll-$level.txt > $FOLDER/out-conll-$level-utf8.txt
+	# 		python $CHANGE $level
+	# 		time python $TRAIN $FOLDER --fileids out-conll-$level-utf8.txt --filename $MODEL/$level"_Maxent.pickle" --reader $READER --classifier Maxent --min_lldelta $i --no-eval
+	# 	done
+	# done
+
+	# min_lldelta with max_iter = 100
+	printf "\n** max_iter = 100 **\n"
 	for i in 0 0.0000001 0.000001 0.00001 0.0001 0.001 0.01 0.05 0.1 0.15 0.2
 	do
 		printf "\n** min_lldelta "$i" **\n"
 		FOLDER=../outputs/repeat-$r/joined
-		MODEL=./models/repeat-$r/experiences/me_min_lldelta/$i
+		MODEL=./models/repeat-$r/experiences/me_min_lldelta-iter-100/$i
 		
 		for level in "${levels[@]}"
 		do
 			iconv -f ISO-8859-1 -t UTF-8 $FOLDER/out-conll-$level.txt > $FOLDER/out-conll-$level-utf8.txt
 			python $CHANGE $level
-			time python $TRAIN $FOLDER --fileids out-conll-$level-utf8.txt --filename $MODEL/$level"_Maxent.pickle" --reader $READER --classifier Maxent --min_lldelta $i --no-eval
+			time python $TRAIN $FOLDER --fileids out-conll-$level-utf8.txt --filename $MODEL/$level"_Maxent.pickle" --reader $READER --classifier Maxent --max_iter 100 --min_lldelta $i --no-eval
 		done
 	done
+
+
 done
