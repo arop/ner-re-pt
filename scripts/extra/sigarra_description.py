254268498cae05acf6e33497cb6773794f6c17f1
arop <andre.r.o.pires@gmail.com>
2017-05-19 17:17:24 +0100

    fix minor bug in sigarra avg chars

--- scripts/extra/sigarra_description.py
+++ scripts/extra/sigarra_description.py
@@ -20,19 +20,21 @@ for uo_dir in os.listdir(data_path):
 			if file.endswith(".ann"):
 				f = open(os.path.join(data_path, uo_dir, file), 'r')
 				entities = []
-				temp_chars = []
 
 				# append all entity tags
 				for l in f.readlines():
 					entities.append(l.split()[1])
-					temp_chars.append(len(l))
 
-				number_chars.append(sum(temp_chars))
 				f.close()
 
 				for ent in entity_tags:
 					entity_count[ent] = entity_count[ent] + entities.count(ent)
 					total_entities[ent] = total_entities[ent] + entities.count(ent)
+
+			elif file.endswith(".txt"):
+				f = open(os.path.join(data_path, uo_dir, file), 'r')
+				number_chars.append(len(f.read()))
+				f.close()
 
 		avg_chars = (sum(number_chars)/len(number_chars))
 		total_avg_chars.append(avg_chars)
